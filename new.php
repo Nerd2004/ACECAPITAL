581596521b3ff89f789aaffa99808f394fb593ad
tusharlikestocode <109097998+tusharlikestocode@users.noreply.github.com>
2022-10-08 11:00:32 +0530

    Add files via upload

--- new.php
+++ new.php
@@ -4,12 +4,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
   <title>ACECAPITAL</title>
   <link rel="stylesheet" href="stockstyle.css">
 </head>
 <body>
 <div class="img">
-  <img src="Images/newlogo.png" id="logo">
+  <img src="Images/newlogo.png" alt="" id="logo">
   <h3>ACECAPITAL</h3>
   </div>
 <?php
